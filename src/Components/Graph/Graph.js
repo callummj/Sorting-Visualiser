670c78c36641f3dc3c8883cb3402d652d8ebb269
Callum <callumj22@gmail.com>
2021-02-22 14:30:31 +0000

    Converted setTimeout animation control to Request Animation frame. This provides better peformance and better reliability across major browsers. Still some extras to do though. Sources I used to learn and for starting code:
    https://developer.mozilla.org/en-US/docs/Web/API/Window/cancelAnimationFrame
    https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame
    
    https://css-tricks.com/using-requestanimationframe-with-react-hooks/

--- src/Components/Graph/Graph.js
+++ src/Components/Graph/Graph.js
@@ -1,167 +1,93 @@
-import React from 'react';
-import {useState, useEffect} from 'react';
-import './Graph.css'
+import {useState, useRef, useEffect} from 'react';
 import Bar from "./Bar/Bar";
+import './Graph.css'
 
+export default function Graph(props){
 
-function Graph(props) {
     let steps = props.steps;
     let [complete, setComplete] = useState(false);
     let [sort, setSort] = useState(props.sort)
-    const [index, setIndex] = useState(0);
+    let [index, setIndex] = useState(0);
     let speed = props.speed;
 
-    useEffect(() => {
 
-
-
-        if ((props.reset == true) &&(index != 0)){
-            console.log("resetting")
-            setIndex(0);
-            setComplete(false)
-            props.resetCompletedCallback();
-        }
-
-        if (props.pause == true){
-            setSort(false);
-        }
-
-        /*
-        if (reset == true && index != 0){
-            alert("resetting index")
-            setIndex(0);
-            setReset = false;
-        }*/
-
-
-        //Sorting status for local array:
-        //If
-        if (props.sort){
-            if (!complete){
-                if (sort != true){
-                    setSort(true)
-                }
-            }else if (complete){
-                props.stopSort()
-                setSort(false)
-            }
-        }
-
-        console.log("algorithm: " + props.title + " index: " + index)
-        console.log("length of steps: " + steps.length + " with index 0: " + steps[0])
-
-        //if props.sorting
-            //if not complete
-                //if sort != true
-
-
-        /*
-
-        OLD
-
-        //If not complete and continue to sort
-        if (!complete && sort){
-            const interval = setInterval(() => {
-                console.log("compolete: " + complete)
-                if (!(complete)) {
-                    if (steps.length > index + 1) {
-                        setIndex(index + 1)
-                    } else {
-                        console.log("should stop sort")
-                        props.stopSort()
-                        setSort(false);
-                        setComplete(true);
-
-                    }
-                }
-            }, speed);
-            return () => clearInterval(interval); //deletes*/
-
-
-
-
-
-
-            if (!complete && sort){
-            const interval = setInterval(() => {
-                return new Promise(resolve => {
-                        console.log("compolete: " + complete)
-
-                        if (!(complete)) {
-                            if (steps.length > index + 1) {
-                                setIndex(index + 1)
-                            } else {
-                                console.log("should stop sort")
-                                props.stopSort()
-                                setSort(false);
-                                setComplete(true);
-                            }
-                        }
-                    }
-                )
-            }, speed);
-            return () => clearInterval(interval); //deletes
-        }
-
-    });
-
-
-    //TODO find out why, after an algorithm has been searched, then generated data again, cannot reanimate. Data does not get to here, and is 'undefined' in console.
-    console.log("reset: " + props.reset + " index: " + index + " vs steps: " + steps.length)
-
-
-
-    //TEMP FIX:
-
-    if (index > steps.length){
-        //alert("index larger than steps.length")
+    //Start the sort
+    if ( (sort == false) && (props.sort == true) ){
+        setSort(true);
+    }else if ( (sort == true) && (props.sort == false) ){
+        setSort(false);
     }
+
+
+
+    // Use useRef for mutable variables that we want to persist
+    // without triggering a re-render on their change
+
+
+    const requestRef = useRef();
+    const previousTimeRef = useRef();
+
+
+
+    useEffect(() => {
+        const animate = time => {
+            if (previousTimeRef.current != undefined) {
+                const deltaTime = time - previousTimeRef.current;
+                setIndex(prevIndex => (prevIndex + 1));
+            }
+            previousTimeRef.current = time;
+            requestRef.current = requestAnimationFrame(animate);
+        }
+
+        if (sort == true && !complete){
+            requestRef.current = requestAnimationFrame(animate);
+            if (index == steps.length-1){
+                setComplete(true);
+            }
+            return () => cancelAnimationFrame(requestRef.current);
+        }
+
+    }, );
+
+
+
+    let dataToPass = steps[index];
+
+
+    //Because of how requestAnimationFrame works, it will loop over one more time
+    if (dataToPass == undefined){
+        dataToPass = steps[index-1];
+    }
+
+
+
+
     return (
-        <div>
+        <div id = {props.graphID}>
             {speed}
-            {drawBars(steps[index], props, props.title, props.decoration, complete)}
+            {drawBars(dataToPass, props, props.title, props.decoration, complete)}
         </div>
     )
-}export default Graph
 
-
+}
 
 const drawBars = (data, props, algorithm, decoration, complete) => {
 
     let focus = [];
 
-    console.log("data here: " + data  + " for alg: " + algorithm)
     //Has a focus
     if (data.length == 2){
-        console.log("data for focus: " + data)
+
         focus = data[1];
         data = [...data[0]]
         // data = [...dta
     }
-
-    console.log("focus here: " + focus)
-
-
-
-    console.log("data: " + data + " for " + props.title);
-
-
     let height = data[0].max + " px";
 
-
     let key = 0;
-    const gg = data.map((value) =>
-        // Correct! Key should be specified inside the array.
-        <>
-
-        <Bar key={key++} value = {value} decoration = {decoration} complete = {complete} focus = {focus}/>
-        </>
-    );
 
     const bars = data.map((value, index) =>
-        // Only do this if items have no stable IDs
         <>
-            {console.log("index in map: " + index + " value in map: " + value)}
             <Bar index={index} value = {value} decoration = {decoration} complete = {complete} focus = {focus}/>
         </>
 
@@ -192,5 +118,4 @@ const drawBars = (data, props, algorithm, decoration, complete) => {
             </div>
         );
     }
-
 }
